8be1cd4647eb24a0e33cf3bf3f5144631535d25c
Vini Falavigna <vini.df@gmail.com>
2020-02-28 16:45:26 +1100

    initial commit

---
+++ webpack-prod.config.js
@@ -0,0 +1,37 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+
+module.exports = {
+  mode: "production",
+
+  resolve: {
+    modules: ["src", "node_modules"],
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.ejs",
+      title: "Weather Widget",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+    ],
+  },
+}
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+
+module.exports = {
+  mode: "development",
+
+  devtool: "source-map",
+
+  resolve: {
+    modules: ["src", "node_modules"],
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.ejs",
+      title: "Weather Widget",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+    ],
+  },
+}
